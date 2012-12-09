8f14301d8ffe87cb98860bdf785a83d1b3c67e5c
Paul Shen <paul@mnml0.com>
2012-12-08 22:16:41 -0800

    Add requirejs app flow

--- node/public/js/main.js
+++ node/public/js/main.js
@@ -1,1 +1,13 @@
+require.config({
+  paths: {
+    jquery: 'vendor/jquery-1.8.0.min',
+    underscore: 'vendor/underscore-1.4.3.min',
+    backbone: 'vendor/backbone-0.9.2.min'
+  }
+});
 
+require([
+  'app',
+], function(App) {
+  App.initialize();
+});
